2e984244e7ffa26cc56ab8edd819364a2e554219
Jonathan Azali <azali.jonathan@gmail.com>
2019-04-29 16:07:02 -0700

    updated with logo

--- portfolio-client/src/Menu.js
+++ portfolio-client/src/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './menu.css';
+import azali_logo from './assets/img/azali_logo.png';
 
 class Menu extends React.Component {
   constructor(props) {
@@ -9,17 +10,16 @@ class Menu extends React.Component {
 
   render() {
     return (
-      <header>
-        <h1 className='name-header'>Jonathan Azali</h1>
+      <header className='menu-header'>
+        <img className='azali-logo' src={azali_logo} alt='azali_logo' />
         <table className='menu-table'>
           <tr className='menu-tr'>
             <td className='menu-td'>About</td>
             <td className='menu-td'>Projects</td>
             <td className='menu-td'>Contact</td>
           </tr>
-      </table>
-    </header>
-      
+        </table>
+      </header>
     );
   }
 }
