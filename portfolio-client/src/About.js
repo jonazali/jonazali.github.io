0c1f9a1b6da5bea3ad079e3bd322f74abb82a97e
Jonathan Azali <azali.jonathan@gmail.com>
2019-04-30 15:58:03 -0700

    initialized pages

---
+++ portfolio-client/src/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <article>
+        <h1>About me</h1>
+        <p>
+          I am a veteran of the United States Marine Corps and an alumni of
+          California State University Northridge. I obtained a bachelor of
+          Science in Computer Information Technology with a Domain Emphasis in
+          Mathematics
+        </p>
+        <p>
+          I have several links below if you are interested in my online presence
+        </p>
+      </article>
+    );
+  }
+}
+
+export default About;
