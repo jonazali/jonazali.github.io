8ac521f227e8ca00da39f905fa550e58ec642b41
Jonathan Azali <azali.jonathan@gmail.com>
2019-05-04 22:35:31 -0700

    updates

---
+++ src/Project_Item.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Project_Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <section className='display-container'>
+        <p>WHY</p>
+      </section>
+    );
+  }
+}
+
+export default Project_Item;
