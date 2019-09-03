95f350fe1df788d60d4491e3e51d288a030e5960
jonazali <azali.jonathan@gmail.com>
2019-09-02 21:32:59 -0700

    updates

---
+++ portfolio-client/src/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Import Components Here
+
+// Improt Styles Here
+
+class Portfolio extends React.Component {
+
+    render(){
+        return(
+            <main>
+                <h2>Testing</h2>
+            </main>
+        )
+    }
+}
+
+export default Portfolio;
