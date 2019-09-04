12550d16989ee17b307c9b583010eb8b5de8532d
jonazali <azali.jonathan@gmail.com>
2019-09-03 22:33:35 -0700

    class names initialized and components created

--- portfolio-client/src/components/Home.js
+++ portfolio-client/src/components/Home.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 // Import Components Here
 
-// Improt Styles Here
+// Import Media Here
+import logo from '../images/Logo.png';
+
+// Import Styles Here
+import '../styles/Home.css';
 
 class Home extends React.Component {
 
     render(){
         return(
-            <main>
-                <h2>Testing</h2>
-            </main>
+            <section className='HomePage'>
+                <p className='HomeDescription'>Hi! My name is Jonathan Azali and I am Full Stack Web Developer!</p>
+                <button className='HomeButton'>Click Here to View My Portfolio</button>
+                <img className='HomeImg' src={logo} alt='logo'/>
+            </section>
         )
     }
 }
