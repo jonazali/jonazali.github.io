6336c2d394dc1e8659cc608e9100106eaa375503
jonazali <azali.jonathan@gmail.com>
2019-09-04 23:08:58 -0700

    updates on homepage

--- portfolio-client/src/components/Home.js
+++ portfolio-client/src/components/Home.js
@@ -13,8 +13,10 @@ class Home extends React.Component {
     render(){
         return(
             <section className='HomePage'>
-                <p className='HomeDescription'>Hi! My name is Jonathan Azali and I am Full Stack Web Developer!</p>
-                <button className='HomeButton'>Click Here to View My Portfolio</button>
+                <div className="HomeDescriptionButtonContainer">
+                    <p className='HomeDescription'>Hi! My name is Jonathan Azali and I am Full Stack Web Developer!</p>
+                    <button className='HomeButton'>Click Here to View My Portfolio</button>
+                </div>
                 <img className='HomeImg' src={logo} alt='logo'/>
             </section>
         )
