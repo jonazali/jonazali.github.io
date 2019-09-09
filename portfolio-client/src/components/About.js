18925e24237615f0bbcd19770ea924da7d35d69b
jonazali <azali.jonathan@gmail.com>
2019-09-08 22:11:58 -0700

    updates

--- portfolio-client/src/components/About.js
+++ portfolio-client/src/components/About.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Import Components Here
-
+import ProfileImg from '../images/Profile.jpg';
 // Import Media Here
 
 // Import Styles Here
@@ -12,6 +12,9 @@ class About extends React.Component {
         return(
             <section className='AboutPage'>
                 <p> I am a web developer who specializes in Full Stack Web Development. I also have an eye for UI/UX design. Check out my list of skills by clicking the tab next to my picture or checkout my online presnce by clicking one of the icons below!</p>
+                <div className='ProfileContainer'>
+                    <img className='Profile' src={ProfileImg} alt='Profile Img'></img>
+                </div>
             </section>
         )
     }
