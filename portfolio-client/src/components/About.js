e7872feb25241e2418738c44e5ed72e3f7d32750
jonazali <azali.jonathan@gmail.com>
2019-09-22 00:52:53 -0700

    about page updated

--- portfolio-client/src/components/About.js
+++ portfolio-client/src/components/About.js
@@ -9,21 +9,24 @@ import Linkedin from '../images/Linkedin.png';
 
 // Import Styles Here
 import '../styles/About.css';
-import {Grid, Image} from 'semantic-ui-react';
+import {Grid, Image, Container} from 'semantic-ui-react';
 
 class About extends React.Component {
 
     render(){
         return(
-            <div id='AboutPage'>
+            <div id='AboutContainer'>
                 <Grid>
-                    <Grid.Row>
-                        <Grid.Column id='AboutDescriptionContainer' width={5}>
-                                <p id='AboutDescription'> I am a web developer who specializes in Full Stack Web Development. I also have an eye for UI/UX design. Check out my list of skills by clicking the tab next to my picture or checkout my online presnce by clicking one of the icons below!</p>
-                                <Grid>
-                                </Grid>
+                    <Grid.Row id='AboutRowContainer'>
+                        <Grid.Column id='DescriptionContainer' width={11}>
+                                <Container text id='Description'> I am a developer who specializes in Full Stack Web Development. I also have an eye for UI/UX design. Check out my list of skills by clicking the tab next to my picture or checkout my online presnce by clicking one of the icons below!</Container>
+                                <Container id='OnlinePresenceContainer'>
+                                    <Image size='small' floated='left' src={Github} alt='Github Img'/>
+                                    <Image size='small' floated='right' src={Linkedin} alt='LinkedIn Img'/>
+                                </Container>
+                               
                         </Grid.Column>
-                        <Grid.Column floated='right' width={5}>
+                        <Grid.Column id='ProfileContainer' floated='right' width={5}>
                             <Image id='Profile' src={ProfileImg} alt='Profile Img' wrapped /> 
                         </Grid.Column>
                         
