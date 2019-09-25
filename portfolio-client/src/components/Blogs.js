d5231c521f6f0818a25a9a5226c599a25c649a37
jonazali <azali.jonathan@gmail.com>
2019-09-24 23:43:26 -0700

    blog initialized

---
+++ portfolio-client/src/components/Blogs.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// Import Components Here
+import { Grid, Button, Image, Rail, Segment, Icon } from 'semantic-ui-react';
+
+// Import Media Here
+
+// Import Styles Here
+import '../styles/Blogs.css'
+
+class Blogs extends React.Component {
+
+    render(){
+        return(
+            <div id='BlogsDiv'>
+                
+            </div>
+        )
+    }
+} 
+
+export default Blogs;
